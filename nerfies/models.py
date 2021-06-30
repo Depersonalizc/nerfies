446dd38ecc28f2f375edc484da481fa9c3a58a45
Depersonalizc <1046770165@qq.com>
2021-07-01 01:54:03 +0800

    Update models.py

--- nerfies/models.py
+++ nerfies/models.py
@@ -14,6 +14,7 @@
 
 """Different model implementation plus a general port for all the models."""
 from typing import Any, Dict, Mapping, Optional, Tuple
+from functools import partial
 
 from flax import linen as nn
 from flax.core.frozen_dict import FrozenDict
@@ -84,19 +85,31 @@ class NerfModel(nn.Module):
   alpha_channels: int
   rgb_channels: int
   use_stratified_sampling: bool
+
+  # Lantent codes
+  num_appearance_embeddings: int      # appearance
+  num_appearance_features: int
+  num_camera_embeddings: int          # camera
+  num_camera_features: int
+  num_warp_embeddings: int            # warp field
+  num_warp_features: int
+  # Let's first reproduce hypernerf:
+  # @ambient_latent = @warp_latent
+  # No need for extra ambient latent
+  num_ambient_embeddings: int         # ambient field
+  num_ambient_features: int
+
+  # Input positional encoding
   num_nerf_point_freqs: int
   num_nerf_viewdir_freqs: int
-
-  num_appearance_embeddings: int
-  num_appearance_features: int
-  num_camera_embeddings: int
-  num_camera_features: int
-  num_warp_embeddings: int
-  num_warp_features: int
   num_warp_freqs: int
+  num_ambient_freqs: int
 
+  num_ambient_dims: int
   activation: types.Activation = nn.relu
   sigma_activation: types.Activation = nn.relu
+
+  # Switches
   use_white_background: bool = False
   use_linear_disparity: bool = False
   use_sample_at_infinity: bool = True
@@ -105,19 +118,24 @@ class NerfModel(nn.Module):
   use_appearance_metadata: bool = False
   use_camera_metadata: bool = False
   use_warp: bool = False
+  use_ambient: bool = False
+  sep_ambient_latent: bool = False  # If True use seperate latent for ambient field
   use_warp_jacobian: bool = False
   use_weights: bool = False
   warp_kwargs: Mapping[str, Any] = FrozenDict()
+  ambient_kwargs: Mapping[str, Any] = FrozenDict()
 
   metadata_encoded: bool = False
 
   def setup(self):
+    # Positional encoding
     self.point_encoder = model_utils.vmap_module(
-        modules.SinusoidalEncoder, num_batch_dims=2)(
+        modules.SinusoidalEncoder, num_batch_dims=2)( # B, S
             num_freqs=self.num_nerf_point_freqs)
     self.viewdir_encoder = model_utils.vmap_module(
-        modules.SinusoidalEncoder, num_batch_dims=1)(
+        modules.SinusoidalEncoder, num_batch_dims=1)( # B
             num_freqs=self.num_nerf_viewdir_freqs)
+    # Embedder
     if self.use_appearance_metadata:
       self.appearance_encoder = glo.GloEncoder(
           num_embeddings=self.num_appearance_embeddings,
@@ -126,7 +144,7 @@ class NerfModel(nn.Module):
       self.camera_encoder = glo.GloEncoder(
           num_embeddings=self.num_camera_embeddings,
           features=self.num_camera_features)
-
+    # Nerf MLPs
     self.nerf_coarse = modules.NerfMLP(
         nerf_trunk_depth=self.nerf_trunk_depth,
         nerf_trunk_width=self.nerf_trunk_width,
@@ -148,7 +166,7 @@ class NerfModel(nn.Module):
           rgb_channels=self.rgb_channels)
     else:
       self.nerf_fine = None
-
+    # Warp field
     if self.use_warp:
       self.warp_field = warping.create_warp_field(
           field_type=self.warp_field_type,
@@ -157,66 +175,119 @@ class NerfModel(nn.Module):
           num_features=self.num_warp_features,
           num_batch_dims=2,
           **self.warp_kwargs)
+    # Ambient field
+    if self.use_ambient:
+      assert self.sep_ambient_latent or self.use_warp, \
+        'Must use_warp if ambient field does not have its own latent.'
+      create_ambient_field = partial(
+          warping.create_warp_field,
+          field_type='ambient',
+          num_freqs=self.num_ambient_freqs,
+          ambient_dims=self.num_ambient_dims,
+          num_batch_dims=2,
+          **self.ambient_kwargs)
+      if self.sep_ambient_latent:
+        self.ambient_field = create_ambient_field(
+            num_embeddings=self.num_ambient_embeddings,
+            num_features=self.num_ambient_features)
+      else:
+        self.ambient_field = create_ambient_field(
+            num_embeddings=self.num_warp_embeddings,
+            num_features=self.num_warp_features)
+      # Whether the warp field should return latent
+      self.warp_ret_latent = self.use_ambient and not self.sep_ambient_latent
 
   def __call__(
       self,
       rays_dict: Dict[str, Any],
-      warp_alpha: float = None,
-      metadata_encoded=False,
-      use_warp=True,
-      deterministic=False,
+      warp_alpha: float,
+      metadata_encoded: bool = False,
+      use_warp: bool = True,
   ):
     """Nerf Model.
 
     Args:
       rays_dict: a dictionary containing the ray information. Contains:
-        'origins': the ray origins.
-        'directions': unit vectors which are the ray directions.
+        'origins': the ray origins. (device_batch, 3)
+        'directions': tip-aligned ray directions. (device_batch, 3)
         'viewdirs': (optional) unit vectors which are viewing directions.
-        'metadata': a dictionary of metadata indices e.g., for warping.
-      warp_alpha: the alpha for the positional encoding.
-      metadata_encoded: if True, assume the metadata is already encoded.
-      use_warp: if True use the warp field (if also enabled in the model).
-      deterministic: whether evaluation should be deterministic.
+        'metadata': a dictionary of metadata indices
+          - warping
+          - appearance
+          Each has shape (device_batch, 1)
+      warp_alpha (float): the alpha for the positional encoding.
+      metadata_encoded (Bool): if True, assume the metadata is already encoded.
+      use_warp (Bool): if True use the warp field (if also enabled in the model).
+      deterministic (Bool): whether evaluation should be deterministic.
 
     Returns:
       ret: list, [(rgb, disp, acc), (rgb_coarse, disp_coarse, acc_coarse)]
     """
-    # Extract viewdirs from the ray array
-    origins = rays_dict['origins']
-    directions = rays_dict['directions']
-    metadata = rays_dict['metadata']
-    if 'viewdirs' in rays_dict:
+    # ------------- RAY EXTRACTION -------------
+    origins = rays_dict['origins']          # (device_batch, 3)
+    directions = rays_dict['directions']    # (device_batch, 3)
+    metadata = rays_dict['metadata']        # {(device_batch, 1/encoded)}
+    if 'viewdirs' in rays_dict:             # (device_batch, 3)
       viewdirs = rays_dict['viewdirs']
     else:  # viewdirs are normalized rays_d
       viewdirs = directions
-    # Stratified sampling along rays
+
+    # -------------- COARSE MODEL --------------
+    # 1. Stratified sampling along rays
+    #    - z_vals.shape: (device_batch, num_course_samples,  )
+    #    - points.shape: (device_batch, num_course_samples, 3)
     z_vals, points = model_utils.sample_along_rays(
         self.make_rng('coarse'), origins, directions, self.num_coarse_samples,
         self.near, self.far, self.use_stratified_sampling,
         self.use_linear_disparity)
+
+    # 2. Apply warping to points.
+    #    - points.shape: (device_batch, num_coarse_samples, 3)
+    #    - warp_jacobian.shape: (device_batch, num_coarse_samples, 3, 3)
     if self.use_warp and use_warp:
       metadata_channels = self.num_warp_features if metadata_encoded else 1
-      warp_metadata = jnp.broadcast_to(
-          metadata['warp'][:, jnp.newaxis, :],
+      warp_metadata = metadata['warp']  # (device_batch, metadata_channels)
+      warp_metadata = jnp.broadcast_to( # (device_batch, num_coarse_samples, 1/encoded)
+          warp_metadata[:, None, :],
           shape=(*points.shape[:2], metadata_channels))
+      # Feed points into the warp_field, which has been vmapped to first 2 
+      # dims (device_batch, num_course_samples) of @points, @warp_metadata.
+      # The warp field does embedding on its own, windowed by @warp_alpha.
       warp_ret = self.warp_field(
-          points, warp_metadata, warp_alpha, self.use_warp_jacobian,
-          metadata_encoded)
+          points, warp_metadata,  # vmapped
+          warp_alpha,
+          metadata_encoded,
+          self.use_warp_jacobian,
+          self.warp_ret_latent)
+      points = warp_ret['warped_points']
       if self.use_warp_jacobian:
-        points, warp_jacobian = warp_ret
-      else:
-        points = warp_ret
+        warp_jacobian = warp_ret['jacobian']
+      if self.warp_ret_latent:
+        warp_latent = warp_ret['latent']
 
+    # 2.5 Predict ambient coordinates.
+    if self.use_ambient:
+      if self.sep_ambient_latent: # TODO: use own embedding
+        pass
+      else:  # use warp latent 
+        ambient_ret = self.ambient_field(
+            points, warp_latent,              # vmapped
+            True, warp_alpha, False, False)   # TODO: Should have own alpha 
+      ambient_w = ambient_ret['ambient_w']
+      print(ambient_w.shape)
+
+    # 3. Apply postional encoding to (warpped) points.
+    #    - points_embed.shape: (device_batch, num_coarse_samples, embedded_dims)
+    #    - point_endcoder vmapped to first 2 dims (device_batch, num_coarse_samples)
     points_embed = self.point_encoder(points)
-
+    
+    # 4. Append condition inputs (encoded viewdir, appearance latent)
+    #    - Each condition has shape (device_batch, embedded_dims)
+    #    - condition_inputs.shape: (device_batch, sum(embedded_dims))
     condition_inputs = []
-
-    # Point attribute predictions
     if self.use_viewdirs:
       viewdirs_embed = self.viewdir_encoder(viewdirs)
       condition_inputs.append(viewdirs_embed)
-
     if self.use_appearance_metadata:
       if metadata_encoded:
         appearance_code = metadata['appearance']
@@ -229,20 +300,25 @@ class NerfModel(nn.Module):
       else:
         camera_code = self.camera_encoder(metadata['camera'])
       condition_inputs.append(camera_code)
-
     # The condition inputs have a shape of (B, C) now rather than (B, S, C)
     # since we assume all samples have the same condition input. We might want
     # to change this later.
+    # Broadcasted to (device_batch, num_coarse_samples, sum(embedded_dims)) 
+    # in the @nerf_coarse MLP.
     condition_inputs = (
         jnp.concatenate(condition_inputs, axis=-1)
         if condition_inputs else None)
 
+    # 5. Query the coarse network
+    #    - coarse_raw: {'rgb'  : (device_batch, num_coarse_samples, 3)
+    #                   'alpha': (device_batch, num_coarse_samples, 1)}
     coarse_raw = self.nerf_coarse(points_embed, condition=condition_inputs)
     # Add noises to regularize the density predictions if needed
     coarse_raw = model_utils.noise_regularize(
         self.make_rng('coarse'), coarse_raw, self.noise_std,
         self.use_stratified_sampling)
-    # Volumetric rendering.
+
+    # 6. Volumetric rendering.
     rgb, exp_depth, med_depth, disp, acc, weights = (
         model_utils.volumetric_rendering(
             coarse_raw,
@@ -251,22 +327,25 @@ class NerfModel(nn.Module):
             use_white_background=self.use_white_background,
             sigma_activation=self.sigma_activation,
             sample_at_infinity=self.use_sample_at_infinity))
-
-    out = {
-        'coarse': {
-            'rgb': rgb,
-            'depth': exp_depth,
-            'med_depth': med_depth,
-            'disp': disp,
-            'acc': acc,
-        }
+    coarse = {
+      'rgb': rgb,               # (device_batch, 3)
+      'depth': exp_depth,       # (device_batch,  )
+      'med_depth': med_depth,   # (device_batch,  )
+      'disp': disp,             # (device_batch,  )
+      'acc': acc                # (device_batch,  )
     }
     if self.use_warp and use_warp and self.use_warp_jacobian:
-      out['coarse']['warp_jacobian'] = warp_jacobian
+      coarse['warp_jacobian'] = warp_jacobian
     if self.use_weights:
-      out['coarse']['weights'] = weights
-    # Hierarchical sampling based on coarse predictions
+      coarse['weights'] = weights
+    out = {'coarse': coarse}
+
+
+    # --------------- FINE MODEL ---------------
     if self.num_fine_samples > 0:
+      # 1. Hierarchical sampling along rays based on coarse weights
+      #    - z_vals.shape: (device_batch, num_fine_samples,  )
+      #    - points.shape: (device_batch, num_fine_samples, 3)
       z_vals_mid = .5 * (z_vals[..., 1:] + z_vals[..., :-1])
       z_vals, points = model_utils.sample_pdf(
           self.make_rng('fine'),
@@ -278,18 +357,50 @@ class NerfModel(nn.Module):
           self.num_fine_samples,
           self.use_stratified_sampling,
       )
+      
+      # 2. Apply warping to points.
+      #    - points.shape: (device_batch, num_fine_samples, 3)
       if self.use_warp and use_warp:
         metadata_channels = self.num_warp_features if metadata_encoded else 1
         warp_metadata = jnp.broadcast_to(
-            metadata['warp'][:, jnp.newaxis, :],
+            metadata['warp'][:, None, :],
             shape=(*points.shape[:2], metadata_channels))
-        points = self.warp_field(
-            points, warp_metadata, warp_alpha, False, metadata_encoded)
+        warp_ret = self.warp_field(
+            points, warp_metadata,  # vmapped
+            metadata_encoded, warp_alpha,
+            False, self.warp_ret_latent)
+        points = warp_ret['warped_points']
+      if self.warp_ret_latent:
+        warp_latent = warp_ret['latent']
+    
+      # 2.5 Predict ambient coordinates.
+      if self.use_ambient:
+        if self.sep_ambient_latent: # TODO: use own embedding
+          pass
+        else:  # use warp latent 
+          ambient_ret = self.ambient_field(
+              points, warp_latent,    # vmapped
+              True, warp_alpha)       # TODO: Should have own alpha 
+        ambient_w = ambient_ret['ambient_w']
+        print(ambient_w.shape)
+      
+      # 3. Apply postional encoding to (warpped) points.
+      #    - points_embed.shape: (device_batch, num_fine_samples, embedded_dims)
+      #    - point_endcoder vmapped to first 2 dims (device_batch, num_fine_samples)
       points_embed = self.point_encoder(points)
+
+      # 4. Append condition inputs (encoded viewdir, appearance latent)
+      #    - SKIPPED. We have the save rays as in coarse model
+      #    - condition_inputs.shape: (device_batch, sum(embedded_dims))
+      # 5. Query the fine network
+      #    - fine_raw: {'rgb'  : (device_batch, num_fine_samples, 3)
+      #                 'alpha': (device_batch, num_fine_samples, 1)}
       fine_raw = self.nerf_fine(points_embed, condition=condition_inputs)
       fine_raw = model_utils.noise_regularize(
           self.make_rng('fine'), fine_raw, self.noise_std,
           self.use_stratified_sampling)
+
+      # 6. Volumetric rendering.
       rgb, exp_depth, med_depth, disp, acc, weights = (
           model_utils.volumetric_rendering(
               fine_raw,
@@ -298,15 +409,16 @@ class NerfModel(nn.Module):
               use_white_background=self.use_white_background,
               sigma_activation=self.sigma_activation,
               sample_at_infinity=self.use_sample_at_infinity))
-      out['fine'] = {
-          'rgb': rgb,
-          'depth': exp_depth,
-          'med_depth': med_depth,
-          'disp': disp,
-          'acc': acc,
+      fine = {
+        'rgb': rgb,
+        'depth': exp_depth,
+        'med_depth': med_depth,
+        'disp': disp,
+        'acc': acc,
       }
       if self.use_weights:
-        out['fine']['weights'] = weights
+        fine['weights'] = weights
+      out['fine'] = fine
 
     return out
 
@@ -317,6 +429,7 @@ def nerf(key,
          num_appearance_embeddings: int,
          num_camera_embeddings: int,
          num_warp_embeddings: int,
+         num_ambient_embeddings: int,
          near: float,
          far: float,
          use_warp_jacobian: bool = False,
@@ -327,9 +440,12 @@ def nerf(key,
     key: jnp.ndarray. Random number generator.
     config: model configs.
     batch_size: the evaluation batch size used for shape inference.
+
     num_appearance_embeddings: the number of appearance embeddings.
     num_camera_embeddings: the number of camera embeddings.
     num_warp_embeddings: the number of warp embeddings.
+    num_ambient_embeddings: the number of ambient embeddings.
+
     near: the near plane of the scene.
     far: the far plane of the scene.
     use_warp_jacobian: if True the model computes and returns the Jacobian of
@@ -376,24 +492,40 @@ def nerf(key,
       use_stratified_sampling=use_stratified_sampling,
       use_white_background=use_white_background,
       use_sample_at_infinity=config.use_sample_at_infinity,
+      use_linear_disparity=use_linear_disparity,
+      use_weights=use_weights,
+      
+      # input postional encoding
       num_nerf_point_freqs=num_nerf_point_freqs,
       num_nerf_viewdir_freqs=num_nerf_viewdir_freqs,
-      use_linear_disparity=use_linear_disparity,
-      use_warp_jacobian=use_warp_jacobian,
-      use_weights=use_weights,
-      use_appearance_metadata=config.use_appearance_metadata,
-      use_camera_metadata=config.use_camera_metadata,
-      use_warp=config.use_warp,
-      num_appearance_embeddings=num_appearance_embeddings,
-      num_appearance_features=config.appearance_metadata_dims,
-      num_camera_embeddings=num_camera_embeddings,
-      num_camera_features=config.camera_metadata_dims,
-      num_warp_embeddings=num_warp_embeddings,
       num_warp_freqs=config.num_warp_freqs,
-      num_warp_features=config.num_warp_features,
+      num_ambient_freqs=config.num_ambient_freqs,
+      # TODO: num_ambient_to_template_freqs
+
+      # warp/ambient fields
+      use_warp=config.use_warp,
+      use_warp_jacobian=use_warp_jacobian,
       warp_field_type=config.warp_field_type,
       warp_kwargs=config.warp_kwargs,
+      use_ambient=config.use_ambient,
+      num_ambient_dims=config.num_ambient_dims,
+      ambient_kwargs=config.ambient_kwargs,
+
+      # latents
+      use_appearance_metadata=config.use_appearance_metadata,
+      num_appearance_embeddings=num_appearance_embeddings,    # specified by datasource at last
+      num_appearance_features=config.appearance_metadata_dims,
+      use_camera_metadata=config.use_camera_metadata,      
+      num_camera_embeddings=num_camera_embeddings,            # specified by datasource at last
+      num_camera_features=config.camera_metadata_dims,
+      num_warp_embeddings=num_warp_embeddings,
+      num_warp_features=config.num_warp_features,
+      num_ambient_embeddings=num_ambient_embeddings,
+      num_ambient_features=config.num_ambient_features,
+      
   )
+
+  print(model)
 
   init_rays_dict = {
       'origins': jnp.ones((batch_size, 3), jnp.float32),
@@ -405,6 +537,15 @@ def nerf(key,
       }
   }
 
+  '''
+  NerfModel.__call__(
+      self,
+      rays_dict: Dict[str, Any],
+      warp_alpha: float = None,
+      metadata_encoded=False,
+      use_warp=True
+  )
+  '''
   key, key1, key2 = random.split(key, 3)
   params = model.init({
       'params': key,
